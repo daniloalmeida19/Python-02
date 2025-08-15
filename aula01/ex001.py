0d7e30d192b648549427efab944c97cdc38b7901
DaniloAlmeida19 <146149116+DaniloAlmeida19@users.noreply.github.com>
2025-08-14 22:27:34 -0300

    teste

--- aula01/ex001.py
+++ aula01/ex001.py
@@ -1,2 +1,9 @@
-print ('Ola, mundo')
-
+nome = str(input("Digite seu nome: "))
+if nome == "Danilo":
+    print("Que nome bonito!")
+elif nome == "pedro" or nome == "Jão" or nome == "joão":
+    print("tenha um bom dia, {}".format(nome))
+elif nome == "Maria":
+    print("Que nome bonito!")
+else:
+    print("Seu nome é tão bonito quanto Danilo!")
