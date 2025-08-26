0f138b9a867a13e9854dab1620cf86b35023ebf1
DaniloAlmeida19 <146149116+DaniloAlmeida19@users.noreply.github.com>
2025-08-25 22:30:42 -0300

    exe 39

--- desafios/exe39.py
+++ desafios/exe39.py
@@ -4,13 +4,20 @@ from datetime import date
 atual = date.today().year
 nasc = int(input('Ano de nascimento: '))
 idade = atual - nasc
+
 if idade < 18:
-    print(f'Você ainda não tem idade para se alistar.')
+    print(f'Você ainda não tem idade para se alistar falta(m) {18 - idade} ano(s).')
+
 elif idade == 18:
     print(f'Você deve se alistar IMEDIATAMENTE!')
+
 elif idade < 18:
-    print(f' Ainda faltam {18 - idade} anos para o alistamento.')
+    print(f' Ainda faltam {idade, ano} anos para o alistamento.')  
+   
 else:
     print(f'Você já deveria ter se alistado há {idade - 18} anos.')
     print(f'Seu alistamento foi em {nasc + 18}.')
+    print(f'Quem nasceu em {nasc} tem {idade} anos em {atual}.')
+
+
 
