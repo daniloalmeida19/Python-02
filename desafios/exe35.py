9eb9fcfca8456de62d9d2c729b667f78147af4a4
DaniloAlmeida19 <146149116+DaniloAlmeida19@users.noreply.github.com>
2025-09-05 08:38:56 -0300

    atualizado

--- desafios/exe35.py
+++ desafios/exe35.py
@@ -3,13 +3,10 @@ r2 = float(input(' segundo segmento: '))
 r3 = float(input(' terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos acima PODEM FORMAR um triângulo:', end='')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR um triângulo!')
 
-    if r1 == r2 == r3:
-        print(' EQUILÁTERO!')
-    elif r1 != r2 != r3 != r1:
-        print(' ESCALENO!')
-    else:
-        print(' ISÓSCELES!')
+    
         
            
         
