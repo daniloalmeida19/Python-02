fb0b80f0f02b5e3e0beb3f4415b53e4e3fc2c643
DaniloAlmeida19 <146149116+DaniloAlmeida19@users.noreply.github.com>
2025-12-12 09:14:10 -0300

    ok

---
+++ desafios/exe46.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+# Inicia a contagem regressiva de 10 até 0.
+for contagem in range(10, -1, -1):
+    print(contagem)
+    sleep(1)  # Pausa de 1 segundo
+
+# Exibe a mensagem de fogos de artifício!
+print('🎆🎇✨ BUM! BUM! POOOW! ✨🎇🎆')
